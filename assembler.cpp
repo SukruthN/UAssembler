de674fa56f47a4c57395878842ff6ef4f8a94816
SukruthN <54316581+SukruthN@users.noreply.github.com>
2020-02-22 11:37:36 +0530

    Update assembler.cpp

--- assembler.cpp
+++ assembler.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 map<string, string> instructions;
+map<string,string>register_map;
+map<string,string>pseudo_instructions;
 FILE *In;
 FILE *Out;
 struct X
@@ -86,8 +88,82 @@ struct I
 };
 
 
+void build_pseudo_instructions()
+{//bulding the pseudo_instructions map
+	//cout<<3<<endl;
+	pseudo_instructions.insert({"cmpd","cmp 0,1,"});
+	pseudo_instructions.insert({"cmpdi","cmpi 0,1,"});
+	pseudo_instructions.insert({"li","addi"});
+	pseudo_instructions.insert({"lis","addis"});
+	pseudo_instructions.insert({"la","addi"});
+	pseudo_instructions.insert({"mr","or"});
+	pseudo_instructions.insert({"subi","addi"});
+	pseudo_instructions.insert({"subis","addis"});
+	pseudo_instructions.insert({"sub","subf"});
+	pseudo_instructions.insert({"xnop","xori"});
+	/*pseudo_instructions.insert({"xor","X"});
+	pseudo_instructions.insert({"xori","D"});
+	pseudo_instructions.insert({"ld","DS"});
+	pseudo_instructions.insert({"lwz","D"});
+	pseudo_instructions.insert({"std","DS"});
+	pseudo_instructions.insert({"stw","D"});
+	pseudo_instructions.insert({"stwu","D"});
+	pseudo_instructions.insert({"lhz","D"});
+	pseudo_instructions.insert({"lha","D"});
+	pseudo_instructions.insert({"sth","D"});
+	pseudo_instructions.insert({"lbz","D"});
+	pseudo_instructions.insert({"stb","D"});*/
+	
+	return;
+}
+
+
+void build_registers()
+{//bulding the register map
+	//cout<<2<<endl;
+	register_map.insert({"1","00001"});
+	register_map.insert({"2","00010"});
+	register_map.insert({"3","00011"});
+	register_map.insert({"4","00100"});
+	register_map.insert({"5","00101"});
+	register_map.insert({"6","00110"});
+	register_map.insert({"7","00111"});
+	register_map.insert({"8","01000"});
+	register_map.insert({"9","01001"});
+	register_map.insert({"10","01010"});
+	register_map.insert({"11","01011"});
+	register_map.insert({"12","01100"});
+	register_map.insert({"13","01101"});
+	register_map.insert({"14","01110"});
+	register_map.insert({"15","01111"});
+	register_map.insert({"16","10000"});
+	register_map.insert({"17","10001"});
+	register_map.insert({"18","10010"});
+	register_map.insert({"19","10011"});
+	register_map.insert({"20","10100"});
+	register_map.insert({"21","10101"});
+	register_map.insert({"22","10110"});
+	register_map.insert({"23","10111"});
+	register_map.insert({"24","11000"});
+	register_map.insert({"25","11001"});
+	register_map.insert({"26","11010"});
+	register_map.insert({"27","11011"});
+	register_map.insert({"28","11100"});
+	register_map.insert({"29","11101"});
+	register_map.insert({"30","11110"});
+	register_map.insert({"31","11111"});
+	register_map.insert({"0","00000"});
+	register_map.insert({"CIA","00000"});
+	register_map.insert({"NIA","00000"});
+	register_map.insert({"SRR0","00000"});
+
+	return;
+}
+
+
 void build_instructions()
 {//bulding the instruction map
+	//cout<<1<<endl;
 	instructions.insert({"add","XO"});
 	instructions.insert({"addi","D"});
 	instructions.insert({"addis","D"});
@@ -127,6 +203,8 @@ void build_instructions()
 	return;
 }
 
+
+
 map<string,int>symtab;//symbol table
 map<string,int>littab;//literal table
 int countd=0;//keeping instruction count in the .data segment
@@ -162,6 +240,27 @@ char check(char input[])
 
 
 	return 'i';
+}
+
+int eliminate_spaces(char input[],int s)
+{
+	int i;
+	for(i=s;input[i];i++)
+	{
+		if(input[i]!=' ')
+			return i;
+	}
+}
+
+
+void transalate(string ins,string format,char input[],int s)
+{
+
+}
+
+void transalate_pseudo(string ins,string act_ins,char input[],int s)
+{
+
 }
 
 void parse_file(FILE *fp,int n)
@@ -255,12 +354,70 @@ void parse_file(FILE *fp,int n)
 
 	else
 	{
+        int ch=getc(fp);
+		if(ch==EOF)
+			return;
+		else
+		{    int ex=0;
+			string copy;
+			copy="\0";
+          char input[10000];
+          input[0]='\0';
+          fscanf(fp,"%[^\n]",input);
 
-            //pass 2
+          char type=check(input);
+         // cout<<type<<endl;
+
+          for(i=0;input[i];i++)
+          	{
+          		copy+=input[i];
+          //cout<<input[i];
+          	}
+           //cout<<endl;
+           if(type=='i')
+           {  ex+=4;
+           if(ex==countt-4)
+           return; 
+           	i=eliminate_spaces(input,0);
+           	string ins;
+           	ins="\0";
+           	for(j=i;input[j]!=' ';j++)
+           	{
+           		ins+=input[j];
+           	}
+
+           	//cout<<ins<<endl;
+           	if(instructions.find(ins)!=instructions.end())
+           	{
+           		string format=instructions[ins];
+           		//cout<<ins<<endl;
+           		transalate(ins,format,input,j);
+           	}
+           	else if(pseudo_instructions.find(ins)!=pseudo_instructions.end())
+           	{
+           		string act_ins=pseudo_instructions[ins];
+           		//cout<<ins<<endl;
+           		transalate_pseudo(ins,act_ins,input,j);
+
+           	}
+           	else
+           	{  //push this into  output file
+           		cout<<"Invalid instruction"<<endl;
+           	}
+
+           }
+           else if(type=='d')
+           {
+           	ex+=4;
+           	if(ex==countt-4)
+           return; 
+           }            
 
 
+parse_file(fp,2);
 
-	}
+	    }
+     }
 }
 
 
@@ -295,6 +452,8 @@ int main(int argc, char const *argv[])
 		}
 
 		build_instructions();
+		build_registers();
+		build_pseudo_instructions();
 
 		// Parse in passes
 
@@ -304,9 +463,11 @@ int main(int argc, char const *argv[])
 		parse_file( fp,passNumber);
 
 		// Rewind input file & start pass 2
-		//rewind(In);
-		/*passNumber = 2;
-		parse_file( passNumber);*/
+		rewind(In);
+		FILE *fp1;
+		fp1=In;
+		passNumber = 2;
+		parse_file( fp1,passNumber);
 
 		// Close files
 		fclose(In);
